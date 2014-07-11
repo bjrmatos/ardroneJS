fef9fa5960b7d00e03b99f41ed547b519f9a3b9c
DiegoUG <diego.uribe.gamez@gmail.com>
2014-07-11 15:55:45 -0500

    move camara feed and socket.io controls to server.js

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-/**
- * Created with JetBrains WebStorm.
- * User: rohitghatol
- * Date: 6/29/13
- * Time: 3:10 PM
- * To change this template use File | Settings | File Templates.
- */
-
-var express = require('express')
-    , app = express()
-    , server = require("http").createServer(app)
-
+// server express
+var express = require('express'),
+	app     = express(),
+	server  = require("http").createServer(app);
+// express public
 app.use(express.static(__dirname + '/public'));
+// camara-feed
+require("dronestream").listen(3001);
+// server soket.io
+var io = require('socket.io').listen(3002);
+io.set('log level', 1);
+// socket.io events
+io.sockets.on('connection', function (socket) {
+    var arDrone = require('ar-drone');
+    var client = arDrone.createClient();
 
+    setInterval(function(){
+        var batteryLevel = client.battery();
+        socket.emit('event', { name: 'battery',value: batteryLevel});
+    },1000);
 
-require("./drone/camera-feed");
-require("./drone/controller");
+    socket.on('event', function (data) {
+        if(data.name=="takeoff"){
+            console.log("Browser asked Ar Drone to Take Off");
+            client.takeoff();
+        }
+        if(data.name=="spin"){
+            console.log("Browser asked Ar Drone to Start Spinning");
+            client.clockwise(1);
+        }
+        if(data.name=="stop"){
+            console.log("Browser asked Ar Drone to Stay and Hover");
+            client.stop();
+        }
+        if(data.name=="land"){
+            console.log("Browser asked Ar Drone to Land");
+            client.land();
+        }
 
+    });
+});
+// start http server
 app.listen(3000);
 
 
